3e8960c9ba789010781fa4ca4a1b1705c55ffc11
Ammar Madni <ammar@tripiq.eu>
2024-08-16 10:26:59 +0100

    wip

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -76,20 +77,10 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Button
+            title="Press me"
+            onPress={async () => await Promise.allSettled([])}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
